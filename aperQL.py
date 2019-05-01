dc9064b1b6a670d5524d68eedf9bcda0098a4243
eakadams <37330078+eakadams@users.noreply.github.com>
2019-05-01 21:26:53 +0200

    Start QL code

---
+++ aperQL.py
@@ -0,0 +1,31 @@
+# aperQL: Quicklook pipeline for Apertif data
+# E. A. K. Adams
+
+__author__ = "E.A.K. Adams"
+
+
+"""
+Script to run components of a quick look pipeline
+Takes a scan number of target field
+Usage: 
+python aperQL <taskID>
+"""
+
+import argparse
+import numpy as np
+
+#Argument parsing
+parser = argparse.ArgumentParser(description='Run Quick Look pipeline')
+parser.add_argument("taskid",help='Task ID of target field')
+parser.add_argument('-N','--number',default=2,
+                    help='Number of random beams')
+args = parser.parse_args()
+
+
+#get random set of beams
+beams = np.random.randit(0,40,args.number)
+
+
+#get data
+#Structure is same as apercal
+
