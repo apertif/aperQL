cca6b64ec0ec6f2a9cde7226e5a92ff7a6a8ec63
eakadams <37330078+eakadams@users.noreply.github.com>
2019-05-02 20:29:48 +0200

    Find calibrator taskIDs

--- aperQL.py
+++ aperQL.py
@@ -6,29 +6,37 @@ __author__ = "E.A.K. Adams"
 
 """
 Script to run components of a quick look pipeline
-Takes a scan number of target field
+Takes a taskid of target field, plus taskids for start/end
+of calibrator scan.
+Future development would have the calibrator scans found
+automatically.
 Usage: 
-python aperQL <taskID>
+python aperQL <taskID> <calstartid> <calendid> -N 10
 """
 
 import argparse
 import numpy as np
+from modules.functions import *
 
 #Argument parsing
 parser = argparse.ArgumentParser(description='Run Quick Look pipeline')
-parser.add_argument("taskid",help='Task ID of target field')
-parser.add_argument("calstart",help='Task ID of start of calibrator scan')
-parser.add_argument("calend",help='Taks ID of end of calibrator scan')
-parser.add_argument('-N','--number',default=2,
+parser.add_argument("taskid",help='Task ID of target field',type=str)
+parser.add_argument("calstart",help='Task ID of start of calibrator scan',type=str)
+parser.add_argument("calend",help='Taks ID of end of calibrator scan',type=str)
+parser.add_argument('-N','--number',default=2,type=int,
                     help='Number of random beams')
 args = parser.parse_args()
 
 
 #get random set of beams
-beams = np.random.randit(0,40,args.number)
+beams = np.random.randint(0,40,args.number)
+
+print beams
 
 #get calibrator taskIDs matched to beams
+cal_tids = get_cal_tids(args.calstart,args.calend,beams)
 
+print cal_tids
 
 #get data
 #Structure is same as apercal
--- modules/functions.py
+++ modules/functions.py
@@ -3,4 +3,49 @@
 
 __author__="E.A.K. Adams"
 
+import numpy as np
 from apercal.subs.getdata_alta import getdata_alta
+
+def get_cal_tids(calstart,calend,beams):
+    """Take starting calibrator taskid, 
+    ending calibrator taskid, and beam list
+    Return list of task ids for the calibrator
+    scans matched to beams
+    """
+    #first check if cal scan crosses a day or not
+    #get date for start & end observation
+    datestart = calstart[0:6]
+    dateend = calend[0:6]
+    cal_tids = np.empty(len(beams),dtype=np.object)
+    if datestart == dateend:
+        #observations on same day
+        #this is the easy case
+        scanstart = int(calstart[-3:])
+        scannums = beams + scanstart
+        for i,scan in enumerate(scannums):
+            cid = '{0}{1:0>3}'.format(datestart,scan)
+            cal_tids[i] = cid
+    else:
+        #cal scans cross a day
+        #this is the complicated case
+        #figure out how many scans are on the end day
+        endscan = int(calend[-3:])
+        #scan numbers start at one
+        #so this is number of scans on second day
+        firstscan = int(calstart[-3:])
+        #last scan number on first day
+        endfirstday = firstscan + 40 - endscan
+        nfirstday = 40-endscan #number scans first day
+        for i,bm in enumerate(beams):
+            if bm < nfirstday:
+                #on first day
+                scan = firstscan+bm
+                cid = '{0}{1:0>3}'.format(datestart,scan)
+                cal_tids[i] = cid
+            else:
+                #on second day
+                scan = (bm +1 ) - nfirstday
+                cid = '{0}{1:0>3}'.format(dateend,scan)
+                cal_tids[i] = cid
+
+    return cal_tids
